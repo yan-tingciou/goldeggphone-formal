7992a698faf7f642d64a66b772fb9a907a26eaba
yan-tingciou <51735813+yan-tingciou@users.noreply.github.com>
2024-05-27 11:09:19 +0800

    commit 8

--- src/MainStyle.js
+++ src/MainStyle.js
@@ -1,5 +1,5 @@
 export const PrimaryColor = '#ff8c00';
-// export const PrimaryColorLighter = 'rgba(119,180,140,0.8)';
-// export const PrimaryColorLightest = 'rgba(119,180,140,0.1)';
 export const SecondaryColor = '#FFD1A4';
+export const ThirdColor = '#D0D0D0';
+
 
--- src/root.jsx
+++ src/root.jsx
@@ -7,7 +7,7 @@ import { PrimaryColor, SecondaryColor } from './MainStyle';
 import Price from './pages/price';
 import Service from './pages/service';
 import About from './pages/about';
-import News from './pages/news';
+// import News from './pages/news';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { Layout, ConfigProvider, FloatButton, Carousel } from "antd";
@@ -46,13 +46,14 @@ const Root = () => {
   const handleMenuOpen = () => {
     if (window.innerWidth < 690) {
       const menu = document.querySelector('.menu');
-      menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+      menu.style.display = menu.style.display !== 'none' ? 'none' : 'flex';
     }
   };
   
   return (
   <ConfigProvider
     theme={{
+      primaryColor: PrimaryColor,
       components: {
         Layout: {
           headerBg: '#000',
@@ -63,10 +64,11 @@ const Root = () => {
         },
         Table:{
           rowHoverBg: SecondaryColor,
-          headerBg: "#000",
-          headerColor: "#fff",
-          headerSortHoverBg: "#D0D0D0",
-          headerColor: PrimaryColor,
+          headerBg: "#D0D0D0",
+          headerColor: "",
+          headerSortHoverBg: PrimaryColor,
+          headerSortActiveBg: PrimaryColor,
+          bodySortBg: PrimaryColor,
         }
       }
     }}
@@ -76,18 +78,18 @@ const Root = () => {
           <div className="demo-logo">
             <h2><Link to="/"></Link></h2>
           </div>
-          <a className="menu-list" ><MenuOutlined onClick={handleMenuOpen}/></a>
+          { window.innerWidth < 690 && <a className="menu-list" ><MenuOutlined onClick={handleMenuOpen}/></a>}
           { window.innerWidth > 690 
             ? 
             <ul className='menu'>
               <li>
-                <Link to={`/service`} className={`link ${location.pathname === '/service' ? 'active' : ''}`} >服務項目</Link>
+                <Link to={`/about`} className={`link ${location.pathname === '/about' ? 'active' : ''}`} >關於金機誕</Link>
               </li>
               <li>
                 <Link to={`/price`} className={`link ${location.pathname === '/price' ? 'active' : ''}`} >維修價目表</Link>
               </li>
               <li>
-                <Link to={`/about`} className={`link ${location.pathname === '/about' ? 'active' : ''}`} >關於金機誕</Link>
+                <Link to={`/service`} className={`link ${location.pathname === '/service' ? 'active' : ''}`} >服務項目</Link>
               </li>
               {/* <li>
                 <Link to={`/news`} className={`link ${location.pathname === '/news' ? 'active' : ''}`} >最新消息</Link>
@@ -100,13 +102,13 @@ const Root = () => {
             ? 
             <ul className='menu small'>
               <li>
-                <Link to={`/service`} className={`link ${location.pathname === '/service' ? 'active' : ''}`} >服務項目</Link>
+                <Link to={`/about`} className={`link ${location.pathname === '/about' ? 'active' : ''}`} >關於金機誕</Link>
               </li>
               <li>
                 <Link to={`/price`} className={`link ${location.pathname === '/price' ? 'active' : ''}`} >維修價目表</Link>
               </li>
               <li>
-                <Link to={`/about`} className={`link ${location.pathname === '/about' ? 'active' : ''}`} >關於金機誕</Link>
+                <Link to={`/service`} className={`link ${location.pathname === '/service' ? 'active' : ''}`} >服務項目</Link>
               </li>
               {/* <li>
                 <Link to={`/news`} className={`link ${location.pathname === '/news' ? 'active' : ''}`} >最新消息</Link>
@@ -177,11 +179,28 @@ const Root = () => {
                 tooltip={<span>加Line聯繫</span>}
               />                  
             </div>
+            <div className="icon_call">
+              <FloatButton
+                href='tel:03-319-7211'
+                target='_blank'
+                shape="circle"
+                style={{
+                  marginTop: '10px',
+                  right: 94,
+                  width: '60px',
+                  height: '60px',
+                }}
+                icon={<i className="fa-solid fa-phone-volume" style={{color: PrimaryColor}}></i>}
+                tooltip={<span>致電金機誕</span>}
+              />                  
+            </div>
           </FloatButton.Group>
         <Footer
           style={{
             textAlign: "center",
-            padding: "15px"
+            padding: "12px",
+            background: "#000",
+            color: PrimaryColor,
           }}
         >
           Copyright © 2016-2024 金機誕 All rights reserved.
